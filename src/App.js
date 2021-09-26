26a5a3d5696cbbfa2c4157ff230c1bc7b8550e73
joeldevel <christianjoel.developer@gmail.com>
2021-09-26 16:10:57 -0300

    add QuoteBox component, App renders several QuoteBox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+// components
+import QuoteBox from './QuoteBox';
+
 function App() {
   return (
     <div>
       <h1>It works</h1>
+      <QuoteBox/>
+      <QuoteBox/>
+      <QuoteBox/>
     </div>
   );
 }
---
+++ src/QuoteBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const QouteBox = () => {
+
+	return (
+
+		<div className="" id="quote-box" 
+			style={{border: "1px dotted purple",margin: 10 }}>
+			<h1>qoutes here</h1>
+			<p id="text">text here</p>
+			<p id="author">author here</p>
+		</div>
+		);
+}
+
+export default QouteBox;

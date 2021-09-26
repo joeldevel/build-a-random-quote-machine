04b3dab02b45aeae87150e5c44b4581f68cd7ff2
joeldevel <christianjoel.developer@gmail.com>
2021-09-26 16:50:16 -0300

    add quote author to QuoteBox

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -6,20 +6,20 @@ import ActionButton from './ActionButton';
 const QouteBox = () => {
 	const [quotes, setQuotes] = useState(null);
 	const [currentQuote, setCurrentQuote] = useState(null);
+
 	useEffect(()=> {
 		fetch("https://type.fit/api/quotes")
 		.then(response=>response.json())
 		.then(data=>{
 			setQuotes(data);
-			// getRandomQuote();
 		})
 		.catch(e=>console.log(e));
 
 	}, []);
 
 	const getRandomQuote = () => {
-		const q = quotes[Math.floor(Math.random()*quotes.length)];
-		setCurrentQuote(q);
+		const randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+		setCurrentQuote(randomQuote);
 	}
 
 	const tweetQuote = () => {
@@ -33,7 +33,7 @@ const QouteBox = () => {
 			<p id="text">{currentQuote? currentQuote.text:
 				quotes? setCurrentQuote(quotes[0]): 
 				"loading"}</p>
-			<p id="author">author here</p>
+			<p id="author">{currentQuote? currentQuote.author: "loading"}</p>
 			<ActionButton btnText="new qoute" id="new-quote" btnType="button" handleClick={getRandomQuote}/>
 			<ActionButton btnText="tweet qoute" id="tweet-quote" btnType="button" handleClick={tweetQuote}/>
 		</div>
